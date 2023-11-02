c6554bc3e217606a04c2bb08e519dd88f9aa129e
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-01 23:56:17 -0300

    feat: setup nest server

--- server/source/index.ts
+++ server/source/index.ts
@@ -1,1 +1,10 @@
-console.log("Its working!!");
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap()
+{
+	const app = await NestFactory.create(AppModule);
+	await app.listen(3000);
+}
+
+bootstrap();
