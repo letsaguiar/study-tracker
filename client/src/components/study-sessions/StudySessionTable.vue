530c7e142d3a66b9865ecfa27526892805fa5a5d
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-11 18:25:03 -0300

    feat: create study session table

---
+++ client/src/components/study-sessions/StudySessionTable.vue
@@ -0,0 +1,52 @@
+<template>
+	<button class="btn btn-success ms-3">New Study Session</button>
+	<table class="table align-items-center justify-content-center mb-0">
+		<thead>
+			<tr>
+				<th id="subject-name" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-80">Subject</th>
+				<th id="subject-name" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-80">Date</th>
+				<th id="subject-name" class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-80">Duration</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="study_session in study_sessions">
+				<td class="px-3">
+					<span class="text-sm fw-bold">{{ study_session.subject.name }}</span>
+				</td>
+				<td class="px-3">
+					<span class="text-sm fw-bold">{{ getStudySessionDate(study_session) }}</span>
+				</td>
+				<td class="px-3">
+					<span class="text-sm fw-bold">{{ getStudySessionDuration(study_session) }}</span>
+				</td>
+				<td class="px-3">
+					<div class="d-flex justify-content-end">
+						<button class="btn btn-primary me-3">edit</button>
+						<button class="btn btn-danger">delete</button>	
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import dayjs from 'dayjs';
+
+function getStudySessionDate(study_session)
+{
+	return (dayjs(study_session.init).format('DD-MM-YYYY'));
+}
+
+function getStudySessionDuration(study_session)
+{
+	const d1 = dayjs(study_session.init);
+	const d2 = dayjs(study_session.end);
+	
+	return (d2.diff(d1, 'minutes') + ' minutes');
+}
+
+const study_sessions = ref([]);
+</script>
