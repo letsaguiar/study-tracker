8216aab8c0bbabc4a32ae3035e0f56769168f834
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-11 18:17:24 -0300

    feat: create study sessions base card

--- client/src/views/StudySessionView.vue
+++ client/src/views/StudySessionView.vue
@@ -1,4 +1,26 @@
-<template></template>
+<template>
+	<div class="container-flud py-4">
+		<div class="row">
+			<div class="col">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div class="card my-4">
+					<div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+						<div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+							<h6 class="text-white text-capitalize ps-3">Study Sessions</h6>
+						</div>
+					</div>
+					<div class="card-body px-0 pb-2">
+						<div class="table-responsive p-0">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
 
 <script setup>
 import { usePathStore } from '../stores/path-store';
