d02669ef00f593917a8f5e720d132e343865fdd1
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-12 17:32:49 -0300

    feat: create study session total duration card

---
+++ client/src/components/home/StudySessionTotalDurationCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card">
+        <div class="card-header p-3 pt-2">
+            <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                <i class="fa-solid fa-clock fa-xl"></i>
+            </div>
+            <div class="text-end pt-1">
+                <p class="text-sm mb-0 text-capitalize">Total Study Time</p>
+                <span class="fs-6 fw-bold mb-0">20 hours and 50 minutes</span>
+            </div>
+        </div>
+        <div class="card-footer p-3"></div>
+    </div>
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <div class="col-3">
+                <StudySessionTotalDurationCard />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import StudySessionTotalDurationCard from '../components/home/StudySessionTotalDurationCard.vue';
+
 import { usePathStore } from '../stores/path-store';
 
 const pathStore = usePathStore();
