00b3193a62bc0fd6075536593f7112a12ee33594
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-18 14:03:36 -0300

    Refactor study session delete modal component

--- client/src/components/study-sessions/StudySessionDeleteModal.vue
+++ client/src/components/study-sessions/StudySessionDeleteModal.vue
@@ -1,24 +1,18 @@
 <template>
-	<div class="modal fade" id="delete-modal" tabindex="-1" data-bs-autohide="true">
-		<div class="modal-dialog modal-dialog-centered">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title">Delete Study Session</h5>
-				</div>
-				<div class="modal-body">
-					<p>Are you sure you want to delete this study session?</p>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn bg-gradient-danger text-white" @click="deleteStudySession()">Yes</button>
-					<button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>
+	<Modal id="study-session-delete-modal" title="Delete Study Session">
+		<template v-slot:body>
+			<p>Are you sure you want to delete this study session?</p>
+		</template>
+		<template v-slot:footer>
+			<button type="button" class="btn bg-gradient-danger text-white" @click="deleteStudySession()">Yes</button>
+			<button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
+		</template>
+	</Modal>
 </template>
 
 <script setup>
-import { Modal } from 'bootstrap';
+import Modal from '../Modal.vue';
+import { Modal as BsModal} from 'bootstrap';
 import { onMounted, watch } from 'vue';
 
 import { StudySessionService } from '../../http/StudySessionService';
@@ -29,7 +23,7 @@ const props = defineProps(['active', 'studySession']);
 let modal;
 
 onMounted(() => {
-	modal = new Modal("#delete-modal");
+	modal = new BsModal("#study-session-delete-modal");
 })
 
 watch(() => props.active, () => {
