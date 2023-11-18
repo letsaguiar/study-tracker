93e4c5eba5fec792c155ce9c3bf4de5f8a49ccc2
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-18 06:39:25 -0300

    Add StudySessionSubjectSelectionModal component

---
+++ client/src/components/study-sessions/create-modal/StudySessionSubjectSelectionModal.vue
@@ -0,0 +1,35 @@
+<template>
+	<Transition>
+		<form @sumit.prevent="next()">
+			<div class="row">
+				<div class="col-12">
+					<div class="input-group input-group-static mb-4">
+						<label class="ms-0">Subject</label>
+						<select class="form-control" v-model="selectedSubject">
+							<option v-for="subject in subjects" :value="subject.id">
+								{{ subject.name }}
+							</option>
+						</select>
+					</div>	
+				</div>
+				<div class="col-9">
+					<button type="submit" class="btn bg-success text-white">Next</button>
+				</div>
+				<div class="col-3">
+					<button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</form>
+	</Transition>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['next']);
+
+const selectedSubject = ref(null);
+
+function next() {
+	emit('next', { id: selectedSubject.value });
+}
+</script>
