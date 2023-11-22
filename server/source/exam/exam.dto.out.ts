3dc1bd854449307be70ffe8bd4fa83e97216d627
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-22 08:03:46 -0300

    create exam entity

---
+++ server/source/exam/exam.dto.out.ts
@@ -0,0 +1,33 @@
+import { IsISO8601, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Exam 
+{
+
+	@PrimaryGeneratedColumn('uuid')
+	@IsUUID()
+	public id: string;
+
+	@Column()
+	@IsString()
+	@IsNotEmpty()
+	public name: string;
+
+	@Column({ nullable: true })
+	@IsISO8601()
+	public first_application_date: string;
+
+	@Column({ nullable: true })
+	@IsISO8601()
+	public second_application_date: string;
+
+	@CreateDateColumn()
+	@IsISO8601()
+	public created_at: string;
+
+	@UpdateDateColumn()
+	@IsISO8601()
+	public updated_at: string;
+
+}
