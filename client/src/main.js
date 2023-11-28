e0d9e22ff0b44a96b94c067c12fe05f4fb2c298f
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-28 11:47:35 -0300

    ci(npm): install fontawasome

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,6 @@
+import './assets/css/tailwind.css';
+
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
 import App from './App.vue';
-import router from './router';
 
-const pinia = createPinia();
-const app = createApp(App);
-
-app.use(router);
-app.use(pinia);
-
-app.mount('#app');
+createApp(App).mount('#app');
