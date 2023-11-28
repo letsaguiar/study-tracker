8ed8117bddbca1a28b778face72e41e6996804ed
Letícia Aguiar <letsaguiar@yandex.com>
2023-11-28 14:27:06 -0300

    feat: create card

---
+++ client/src/components/base/card/Card.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="flex flex-wrap mt-4">
+		<div class="w-full mb-12 px-4">
+			<div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
+				<div class="rounded-t mb-0 px-4 py-3 border-0">
+					<div class="flex flex-wrap items-center">
+						<div class="relative w-full px-4 max-w-full flex-grow flex-1">
+							<h3 class="font-semibold text-lg text-blueGray-700">
+								{{ title }}
+							</h3>
+						</div>
+					</div>
+				</div>
+				<div class="block w-full overflow-x-auto">
+					<slot></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+
+	props: ['title']
+
+};
+
+</script>
--- client/src/views/SubjectView.vue
+++ client/src/views/SubjectView.vue
@@ -1,18 +1,21 @@
 <template>
 	<Main>
-		<Button color="amber" custom-class="mt-5" @click="console.log('foo')">
+		<Button color="amber" custom-class="my-5 mx-4" @click="console.log('foo')">
 			Nova Matéria
 		</Button>
+
+		<Card title="Matérias"></Card>
 	</Main>
 </template>
 
 <script>
 import Button from '../components/base/button/Button.vue';
+import Card from '../components/base/card/Card.vue';
 import Main from '../components/base/main/Main.vue';
 
 export default {
 
-	components: { Main, Button },
+	components: { Main, Button, Card },
 
 }
 
