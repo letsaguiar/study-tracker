591fd6b4d80b554f7be065938207f9b00bba7c6b
Letícia Aguiar <letsaguiar@yandex.com>
2023-12-03 18:17:52 -0300

    finish sidebar

--- client/src/components/sidebar/Sidebar.vue
+++ client/src/components/sidebar/Sidebar.vue
@@ -1,9 +1,17 @@
 <template>
 	<Sidebar>
 		<template #header>
-			<RouterLink to="/" class="flex items-center justify-center uppercase text-pink-500 p-5">
-				<span class="font-bold">Study Tracker</span>
+			<RouterLink to="/" class="flex items-center uppercase text-pink-700 px-3 py-7">
+				<span class="font-thin">study</span>
+				<span class="font-bold">tracker</span>
 			</RouterLink>
+		</template>
+		<template #body>
+			<SidebarItem to="/" icon="fa-solid fa-gauge" name="dashboard" />
+			<SidebarItem to="/subjects" icon="fa-solid fa-bookmark" name="matérias" />
+			<SidebarItem to="/study-sessions" icon="fa-solid fa-clock" name="sessões de estudo" />
+			<SidebarItem to="/exams" icon="fa-solid fa-rectangle-list" name="exames" />
+			<SidebarItem to="/statistics" icon="fa-solid fa-chart-simple" name="estatísticas" />
 		</template>
 	</Sidebar>
 </template>
