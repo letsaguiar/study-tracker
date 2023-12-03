e5d28167ded1a2f84e127bcb4a6d78694af46b4f
Letícia Aguiar <letsaguiar@yandex.com>
2023-12-03 18:25:49 -0300

    create subject view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
 			name: 'home',
 			component: HomeView
 		},
+		{
+			path: '/subjects',
+			name: 'subjects',
+			component: import('../views/SubjectView.vue'),
+		},
 	]
 });
 
