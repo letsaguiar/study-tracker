577d8ecc068acfc890fb779cc61706a5b612fb6b
Letícia Aguiar <letsaguiar@yandex.com>
2023-12-04 17:10:37 -0300

    feat(study-session): add create button to view

--- client/src/views/StudySessionView.vue
+++ client/src/views/StudySessionView.vue
@@ -1,2 +1,34 @@
 <template>
+	<Button color="pink" class="relative mb-5 border-2 border-white" @click="openCreateModal">nova sessão de estudos</Button>
+
+	<Card title="Sessões de Estudo"></Card>
+
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from '@/components/base/button/Button.vue';
+import Card from '../components/base/card/Card.vue';
+
+export default defineComponent({
+	
+	components: { Button, Card },
+
+	data()
+	{
+		return {
+			createModalActive: 0
+		};
+	},
+
+	methods: {
+
+		openCreateModal() {
+			this.createModalActive++;
+		}
+
+	}
+
+});
+
+</script>
