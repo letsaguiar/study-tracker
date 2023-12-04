2f49812667f8f4f71b560393ed6c55e7ccf5682c
Letícia Aguiar <letsaguiar@yandex.com>
2023-12-04 13:58:30 -0300

    feat: create table

---
+++ client/src/components/base/table/Table.vue
@@ -0,0 +1,12 @@
+<template>
+	<table class="w-full text-sm text-left rtl:text-right text-gray-500 rounded-lg">
+		<thead class="text-xs text-gray-700 font-bold uppercase bg-gray-50">
+			<tr>
+				<slot name="head"></slot>
+			</tr>
+		</thead>
+		<tbody>
+			<slot name="body"></slot>
+		</tbody>
+	</table>
+</template>
